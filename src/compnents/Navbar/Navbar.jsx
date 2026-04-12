92f6945fad3473f9107c003e899ab48cf10aaa7c
Sayed <sayedrkhalid.official@gmail.com>
2026-04-12 13:03:20 +0600

    feat: add navbar left and center feature
     - Navbar Left: Add Dropdwon menu and Brand
    
     - Navbar Center: Add Search input and product list

--- src/compnents/Navbar/Navbar.jsx
+++ src/compnents/Navbar/Navbar.jsx
@@ -1,61 +1,107 @@
-import { BiCart, BiMenuAltLeft, BiSearch, BiUser } from "react-icons/bi";
-import { TfiMenuAlt } from "react-icons/tfi";
+import Link from "next/link";
+import {
+  BiCart as ChartIcon,
+  BiMenuAltLeft as MenuIcon,
+  BiSearch as SearchIcon,
+  BiUser as UserIcon,
+  BiListUl as ListIcon,
+} from "react-icons/bi";
 
 const Navbar = () => {
   return (
-    <nav className="flex justify-between items-center py-4">
-      {/* Navbar left */}
-      <div className="flex items-center gap-2">
-        <span className="bg-gray-200 hover:bg-gray-300/65 text-blue-500 p-1 rounded-md cursor-pointer">
-          <BiMenuAltLeft className="w-6 h-6 stroke-[0.3]"></BiMenuAltLeft>
-        </span>
-        <h1 className="text-2xl text-blue-500 font-semibold select-none">
-          <a href="#">TeckNest</a>
-        </h1>
-      </div>
+    <nav>
+      {/* Navbr content */}
+      <div className="navbar bg-base-100 shadow-sm">
+        {/* Navbar Start */}
+        <div className="navbar-start gap-2">
+          {/* Dropdown menu */}
+          <div className="dropdown">
+            <div
+              tabIndex={0}
+              role="button"
+              className="text-gray-300 bg-gray-100/5 hover:bg-gray-100/10 p-2 rounded-md"
+            >
+              <MenuIcon size={24} />
+            </div>
+            <ul
+              tabIndex="-1"
+              className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow"
+            >
+              <li>
+                <a>Homepage</a>
+              </li>
+              <li>
+                <a>Portfolio</a>
+              </li>
+              <li>
+                <a>About</a>
+              </li>
+            </ul>
+          </div>
 
-      {/* Navbar center */}
-      <legend className="flex items-center gap-2 bg-gray-200 rounded-md overflow-hidden">
-        <label htmlFor="search-item" className="relative flex items-center">
-          <span className="absolute ml-2">
-            <BiSearch className="w-5 h-5 stroke-[0.3] fill-gray-400 stroke-gray-400"></BiSearch>
-          </span>
-          <input
-            type="search"
-            name="search-item"
-            id="search-item"
-            placeholder="Search assensials, accesories and more..."
-            className="w-100 h-full pl-8 focus:outline-none"
-          />
-        </label>
-
-        <span className="p-2 bg-gray-300/30 hover:bg-gray-300/65 cursor-pointer">
-          <TfiMenuAlt className="w-5 h-5 fill-gray-600 stroke-gray-600"></TfiMenuAlt>
-        </span>
-      </legend>
-
-      {/* Navbar right */}
-      <div className="flex items-center gap-4">
-        <div className="flex items-center gap-2">
-          <span className="">
-            <BiUser className="w-6 h-6 stroke-[0.3]"></BiUser>
-          </span>
-          <span>
-            <a href="#" className="hover:underline">
-              Sign In
-            </a>
-            /
-            <a href="#" className="hover:underline">
-              Sign Up
-            </a>
-          </span>
+          <Link href="/" className="text-xl font-bold">
+            MegaMart
+          </Link>
         </div>
 
-        <span className="h-6 w-0.5 rounded-full bg-gray-300/65"></span>
+        {/* Navbar Center */}
+        <div className="navbar-center w-[35%]">
+          <div className="input w-full gap-0 p-0 pl-2">
+            <label className="w-full flex gap-2 items-center border-r border-gray-500">
+              <SearchIcon size={24} />
+              <input
+                className="w-full"
+                type="search"
+                required
+                placeholder="Search assentials, groceries and more..."
+              />
+            </label>
 
-        <div className="flex items-center gap-2">
-          <BiCart className="w-6 h-6 stroke-[0.3]"></BiCart>
-          <span>Cart</span>
+            <div className="text-gray-300 cursor-pointer hover:bg-gray-100/5 p-2">
+              <ListIcon size={24} />
+            </div>
+          </div>
+        </div>
+
+        {/* Navbar End */}
+        <div className="navbar-end">
+          <button className="btn btn-ghost btn-circle">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-5 w-5"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              {" "}
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
+              />{" "}
+            </svg>
+          </button>
+          <button className="btn btn-ghost btn-circle">
+            <div className="indicator">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-5 w-5"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                {" "}
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M15 17h5l-1.405-1.405A2.032 2.032 0 0118 14.158V11a6.002 6.002 0 00-4-5.659V5a2 2 0 10-4 0v.341C7.67 6.165 6 8.388 6 11v3.159c0 .538-.214 1.055-.595 1.436L4 17h5m6 0v1a3 3 0 11-6 0v-1m6 0H9"
+                />{" "}
+              </svg>
+              <span className="badge badge-xs badge-primary indicator-item"></span>
+            </div>
+          </button>
         </div>
       </div>
     </nav>
